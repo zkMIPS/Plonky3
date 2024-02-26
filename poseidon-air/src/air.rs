94526a584cb50a4eef0d35d88dc93aaa48c59bca
Nicholas Ward <npward@berkeley.edu>
2024-02-26 13:03:28 -0800

    progress

--- poseidon-air/src/air.rs
+++ poseidon-air/src/air.rs
@@ -1,24 +1,47 @@
+use core::borrow::Borrow;
+
+use alloc::vec::Vec;
 use p3_air::{Air, AirBuilder, BaseAir};
+use p3_field::AbstractField;
+use p3_matrix::MatrixRowSlices;
 
-use crate::columns::{PoseidonCols, NUM_POSEIDON_COLS_WIDTH_8};
+use crate::columns::{get_num_poseidon_cols, PoseidonCols};
 use crate::round_flags::eval_round_flags;
+use crate::{HALF_N_FULL_ROUNDS, N_PARTIAL_ROUNDS};
 
-pub struct PoseidonAirWidth8 {}
+pub struct PoseidonAir<const WIDTH: usize> {
+    pub(crate) round_constants: Vec<u64>,
+}
 
-impl<F> BaseAir<F> for PoseidonAirWidth8 {
+impl<F, const WIDTH: usize> BaseAir<F> for PoseidonAir<WIDTH> {
     fn width(&self) -> usize {
-        NUM_POSEIDON_COLS
+        get_num_poseidon_cols(WIDTH)
     }
 }
 
-impl<AB: AirBuilder> Air<AB> for PoseidonAirWidth8 {
+impl<AB: AirBuilder, const WIDTH: usize> Air<AB> for PoseidonAir<WIDTH> {
     fn eval(&self, builder: &mut AB) {
-        eval_round_flags(builder);
+        eval_round_flags::<AB, WIDTH>(builder);
+        
+        let num_rounds = 2 * HALF_N_FULL_ROUNDS + N_PARTIAL_ROUNDS;
 
         let main = builder.main();
-        let local: &PoseidonCols<AB::Var> = main.row_slice(0).borrow();
-        let next: &PoseidonCols<AB::Var> = main.row_slice(1).borrow();
+        let local: &PoseidonCols<AB::Var, WIDTH> = main.row_slice(0).borrow();
+        let next: &PoseidonCols<AB::Var, WIDTH> = main.row_slice(1).borrow();
 
+        // check that round constants are added correctly
+        let constants = self.round_constants.clone();
+        for i in 0..WIDTH {
+            let mut round_constant = AB::Expr::zero();
+            for r in 0..num_rounds {
+                let this_round = local.round_flags[r];
+                let this_round_constant = AB::Expr::from_canonical_u64(constants[r * WIDTH + i]);
+                round_constant += this_round * this_round_constant;
+            }
+            let current = local.start_of_round[i];
+            let next = local.after_constants[i];
 
+            builder.assert_eq(next, current + round_constant);
+        }
     }
 }
--- poseidon-air/src/columns.rs
+++ poseidon-air/src/columns.rs
@@ -1,4 +1,4 @@
-use core::{borrow::Borrow, mem::{size_of, transmute}};
+use core::{borrow::{Borrow, BorrowMut}, mem::{size_of, transmute}};
 
 use p3_util::indices_arr;
 
@@ -8,10 +8,10 @@ pub(crate) struct PoseidonCols<T, const WIDTH: usize> {
     /// The `i`th value is set to 1 if we are in the `i`th round, otherwise 0.
     pub round_flags: [T; N_ROUNDS],
 
-    pub start_of_round: [[T; WIDTH]; N_ROUNDS],
-    pub after_constants: [[T; WIDTH]; N_ROUNDS],
-    pub after_sbox: [[T; WIDTH]; N_ROUNDS],
-    pub after_mds: [[T; WIDTH]; N_ROUNDS],
+    pub start_of_round: [T; WIDTH],
+    pub after_constants: [T; WIDTH],
+    pub after_sbox: [T; WIDTH],
+    pub after_mds: [T; WIDTH],
 }
 
 pub(crate) const fn get_num_poseidon_cols(width: usize) -> usize {
@@ -24,10 +24,10 @@ pub(crate) const fn get_num_poseidon_cols(width: usize) -> usize {
     }
 }
 
-const fn make_col_map(width: usize) -> PoseidonCols<usize, 8> {
-    if width == 8 {
+const fn make_col_map<const WIDTH: usize>() -> PoseidonCols<usize, WIDTH> {
+    if WIDTH == 8 {
         make_col_map_width_8()
-    } else if width == 12 {
+    } else if WIDTH == 12 {
         make_col_map_width_12()
     } else {
         unimplemented!()
@@ -35,22 +35,33 @@ const fn make_col_map(width: usize) -> PoseidonCols<usize, 8> {
 }
 
 const fn make_col_map_width_8() -> PoseidonCols<usize, 8> {
-    let indices_arr = indices_arr::<NUM_POSEIDON_COLS_WIDTH_8>();
-    unsafe { transmute::<[usize; NUM_POSEIDON_COLS_WIDTH_8], PoseidonCols<usize, 8>>(indices_arr) }
+    let indices_arr = indices_arr::<{get_num_poseidon_cols(8)}>();
+    unsafe { transmute::<[usize; size_of::<PoseidonCols<u8, 8>>()], PoseidonCols<usize, 8>>(indices_arr) }
 }
 
 const fn make_col_map_width_12() -> PoseidonCols<usize, 12> {
-    let indices_arr = indices_arr::<NUM_POSEIDON_COLS_WIDTH_12>();
-    unsafe { transmute::<[usize; NUM_POSEIDON_COLS_WIDTH_12], PoseidonCols<usize, 12>>(indices_arr) }
+    let indices_arr = indices_arr::<{size_of::<PoseidonCols<u8, 12>>()}>();
+    unsafe { transmute::<[usize; size_of::<PoseidonCols<u8, 12>>()], PoseidonCols<usize, 12>>(indices_arr) }
 }
 
-impl<T> Borrow<PoseidonCols<T>> for [T] {
-    fn borrow(&self) -> &PoseidonCols<T> {
-        debug_assert_eq!(self.len(), NUM_POSEIDON_COLS);
-        let (prefix, shorts, suffix) = unsafe { self.align_to::<PoseidonCols<T>>() };
+impl<T, const WIDTH: usize> Borrow<PoseidonCols<T, WIDTH>> for [T] {
+    fn borrow(&self) -> &PoseidonCols<T, WIDTH> {
+        debug_assert_eq!(self.len(), get_num_poseidon_cols(WIDTH));
+        let (prefix, shorts, suffix) = unsafe { self.align_to::<PoseidonCols<T, WIDTH>>() };
         debug_assert!(prefix.is_empty(), "Alignment should match");
         debug_assert!(suffix.is_empty(), "Alignment should match");
         debug_assert_eq!(shorts.len(), 1);
         &shorts[0]
+    }
+}
+
+impl<T, const WIDTH: usize> BorrowMut<PoseidonCols<T, WIDTH>> for [T] {
+    fn borrow_mut(&mut self) -> &mut PoseidonCols<T, WIDTH> {
+        debug_assert_eq!(self.len(), get_num_poseidon_cols(WIDTH));
+        let (prefix, shorts, suffix) = unsafe { self.align_to_mut::<PoseidonCols<T, WIDTH>>() };
+        debug_assert!(prefix.is_empty(), "Alignment should match");
+        debug_assert!(suffix.is_empty(), "Alignment should match");
+        debug_assert_eq!(shorts.len(), 1);
+        &mut shorts[0]
     }
 }
